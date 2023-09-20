77dcfa5218565c97516f4460e44cec17cdd04296
Prophantasia <98701412+Prophantasia@users.noreply.github.com>
2023-09-20 13:33:59 -0300

    Add files via upload

---
+++ ADO3 - Média/src/NotaDisciplina.java
@@ -0,0 +1,31 @@
+public class NotaDisciplina {
+
+    private String disciplina;
+    private double media;
+    private int faltas;
+
+    public String getDisciplina() {
+        return disciplina;
+    }
+
+    public void setDisciplina(String disciplina) {
+        this.disciplina = disciplina;
+    }
+
+    public double getMedia() {
+        return media;
+    }
+
+    public void setMedia(double media) {
+        this.media = media;
+    }
+
+    public int getFaltas() {
+        return faltas;
+    }
+
+    public void setFaltas(int faltas) {
+        this.faltas = faltas;
+    }
+
+}
